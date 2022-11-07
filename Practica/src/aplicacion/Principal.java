59cf177fe7b4c64f0ac2b6515aa1186bdd56a37f
pablo torres <tpablo851@gmail.com>
2022-11-07 01:12:33 +0100

    hh

--- Practica/src/aplicacion/Principal.java
+++ Practica/src/aplicacion/Principal.java
@@ -6,9 +6,11 @@ import interfaz.*;
 public class Principal{
 	public static void main(String args[]){
 		System.out.println("Hola Mundo");
-		int i= 10;
-		float f= i + 2;
-		System.out.println(f);
+		Libreta libreta = new Libreta();
+
+		libreta.setId(9);
+		System.out.println(libreta);
+
 		
 	}
 
